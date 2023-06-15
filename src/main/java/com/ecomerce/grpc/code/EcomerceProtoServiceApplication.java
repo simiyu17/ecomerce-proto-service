c97c8094fae1a308d0537e0cb043d9731f109c74
Daniel Simiyu <simiyuwdan@gmail.com>
2023-06-15 17:17:30 +0300

    Initial push

---
+++ src/main/java/com/ecomerce/grpc/code/EcomerceProtoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecomerce.grpc.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceProtoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceProtoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecomerce/grpc/code/EcomerceProtoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerce.grpc.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceProtoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
